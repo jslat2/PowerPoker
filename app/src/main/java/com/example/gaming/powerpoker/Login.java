4f77e9eee61e5b570f0d32ac0725df2cb4d89844
Jordan Slattery <jslat2@gmail.com>
2015-07-24 03:18:13 -0700

    first commit

---
+++ app/src/main/java/com/example/gaming/powerpoker/Login.java
@@ -0,0 +1,18 @@
+package com.example.gaming.powerpoker;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gaming on 7/23/2015.
+ */
+public class Login extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.login, container, false);
+    }
+}
